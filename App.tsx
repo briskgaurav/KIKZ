da4f69144423cdfe8457c4fc25c413426fd8cf52
Gaurav <briskgaurav@gmail.com>
2025-03-01 22:57:05 +0530

    Setup

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import './global.css'
+
+const App = () => {
+  return (
+    <View className='flex items-center justify-center flex-1'>
+      <Text className='text-blue-600'>my</Text>
+    </View>
+  )
+}
+
+export default App
